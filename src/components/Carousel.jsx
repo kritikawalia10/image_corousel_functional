1ccc47e3bfd2f6a48d2308528d0ec43a210e79a2
Kritika Walia <kritikawalia987@gmail.com>
2023-12-02 16:08:11 +0530

    Add

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,28 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
-}
+    const [newImage, setnewImage] = useState(0);
+    const { img, title, subtitle } = images[newImage];
+  
+    const updateImage = (newIndex) => setnewImage(Math.max(0, Math.min(newIndex, images.length - 1)));
+  
+    return (
+      <div className="carousel">
+        <div className="carouselInner" style={{ backgroundImage: `url(${img})` }}>
+          <div className="left" onClick={() => updateImage(newImage - 1)}>
+            <ArrowBackIosIcon style={{ fontSize: 30 }} />
+          </div>
+          <div className="center">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+          </div>
+          <div className="right" onClick={() => updateImage(newImage + 1)}>
+            <ArrowForwardIosIcon style={{ fontSize: 30 }} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 
 export default Carousel;
